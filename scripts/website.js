98f5e4777c93c05e92a3095ca3fcec46f1e13d4c
puneetxp <puneetsharma9@hotmail.com>
2023-06-10 20:38:11 +0530

    publish with error

--- scripts/website.js
+++ scripts/website.js
@@ -1,6 +1,10 @@
-export const website = {"live":[
-    {"label":"intaxing","website":"intaxing.in"},
-    {"label":"intax","website":"intax.in"},
-    {"label":"velladost","website":"velladost.com"},
-    {"label":"eliteonrent","website":"eliteonrent.com"},
-    {"label":"ikrent","website":"ikrent.com"}]};
+export const website = {
+    "live": [
+        { "label": "intaxing", "website": "intaxing.in" },
+        { "label": "velladost", "website": "velladost.com" },
+        { "label": "eliteonrent", "website": "eliteonrent.com" },
+        { "label": "ikrent", "website": "ikrent.com" }],
+    "dev": [
+        { "label": "intax", "website": "intax.in" }
+    ]
+};
