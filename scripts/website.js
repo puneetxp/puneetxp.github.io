d7da814c48fc71dd0a927b7eb1bd61dca5f4b938
Puneetxp <puneetsharma9@hotmail.com>
2023-12-30 06:44:38 +0000

    website update

--- scripts/website.js
+++ scripts/website.js
@@ -1,13 +1,13 @@
 export const website = {
     "live": [
         { "label": "intaxing", "website": "intaxing.in" },
-        { "label": "velladost", "website": "velladost.com" },
+        { "label": "Zyvika", "website": "zyvika.com" },
         { "label": "eliteonrent", "website": "eliteonrent.com" },
         { "label": "ikrent", "website": "ikrent.com" }],
     "dev": [
         { "label": "intax", "website": "intax.in" },
         { "label": "mfdrent", "website": "mfdrent.in" },
-        { "label": "Devloper Version Velladost", "website": "dev.velladost.com" },
+        { "label": "Devloper NewCaseMart", "website": "newcasemart.com" },
         { "label": "The Documentation", "website": "the-doc.pages.dev" },
     ],
     "partial": [
