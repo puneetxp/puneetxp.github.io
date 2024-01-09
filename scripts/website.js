a4b9cfe26976241146b566f0b2729cde5b9b96f2
Puneetxp <puneetsharma9@hotmail.com>
2024-01-09 10:36:35 +0530

    Update website.js

--- scripts/website.js
+++ scripts/website.js
@@ -2,6 +2,7 @@ export const website = {
     "live": [
         { "label": "intaxing", "website": "intaxing.in" },
         { "label": "Zyvika", "website": "zyvika.com" },
+        { "label": "Zyvika", "website": "app.zyvika.com" },
         { "label": "eliteonrent", "website": "eliteonrent.com" },
         { "label": "ikrent", "website": "ikrent.com" }],
     "dev": [
