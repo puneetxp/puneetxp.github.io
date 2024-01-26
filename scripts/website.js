ac9c52769db0da4f24fd2023beeddd59ed1882f0
Puneetxp <puneetsharma9@hotmail.com>
2024-01-26 09:57:53 +0530

    website update

--- scripts/website.js
+++ scripts/website.js
@@ -6,6 +6,7 @@ export const website = {
         { "label": "eliteonrent", "website": "eliteonrent.com" },
         { "label": "ikrent", "website": "ikrent.com" }],
     "dev": [
+        { "label": "Test Zyvika", "website": "test-app.zyvika.com.s3-website.ap-south-1.amazonaws.com/" },
         { "label": "intax", "website": "intax.in" },
         { "label": "mfdrent", "website": "mfdrent.in" },
         { "label": "Devloper NewCaseMart", "website": "newcasemart.com" },
